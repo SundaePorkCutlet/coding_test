87adc20baf191518660dddfa1ee17e055b9554f1
SundaePorkCutlet <wns301@naver.com>
2024-05-14 12:09:30 +0900

    fix : make function

--- cote2296/main.go
+++ cote2296/main.go
@@ -9,7 +9,6 @@ import (
 )
 
 var buildingCount int
-var quarter [2]int
 var dp [][2]int
 var buildings []building
 
@@ -55,7 +54,6 @@ func main() {
 
 	}
 
-	//1,3분면 최대값
 	sort.Slice(buildings, func(i, j int) bool {
 		if buildings[i].x == buildings[j].x {
 			return buildings[i].y < buildings[j].y
@@ -65,18 +63,8 @@ func main() {
 
 	result := 0
 
-	for i := 0; i < buildingCount; i++ {
-		dp[i][0] = buildings[i].profit
-		for j := 0; j < i; j++ {
-			if buildings[j].y < buildings[i].y {
-				dp[i][0] = max(dp[i][0], dp[j][0]+buildings[i].profit)
-			}
+	result = max(result, getResult(0))
 
-		}
-		result = max(result, dp[i][0])
-	}
-
-	//2,4분면 최대값
 	sort.Slice(buildings, func(i, j int) bool {
 		if buildings[i].x == buildings[j].x {
 			return buildings[i].y < buildings[j].y
@@ -84,16 +72,23 @@ func main() {
 		return buildings[i].x > buildings[j].x
 	})
 
-	for i := 0; i < buildingCount; i++ {
-		dp[i][1] = buildings[i].profit
-		for j := 0; j < i; j++ {
-			if buildings[j].y < buildings[i].y {
-				dp[i][1] = max(dp[i][1], dp[j][1]+buildings[i].profit)
-			}
-		}
-		result = max(result, dp[i][1])
-	}
+	result = max(result, getResult(1))
 
 	fmt.Println(result)
 
+}
+
+func getResult(category int) int {
+	result := 0
+	for i := 0; i < buildingCount; i++ {
+		dp[i][category] = buildings[i].profit
+		for j := 0; j < i; j++ {
+			if buildings[j].y < buildings[i].y {
+				dp[i][category] = max(dp[i][category], dp[j][category]+buildings[i].profit)
+			}
+		}
+		result = max(result, dp[i][category])
+	}
+
+	return result
 }
